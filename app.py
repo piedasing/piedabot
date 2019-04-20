38a6dfbb1aded10dec7bbdf01922a879cc4a0e8a
pieda <pieda@coder.com.tw>
2019-04-20 14:39:16 +0800

    update

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=weather(event.message.text))
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
